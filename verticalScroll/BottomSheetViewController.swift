c661f5d07440c0a82ad4f755cb417a57a5780f94
yasinehsan <yasinehsan11@gmail.com>
2020-01-25 02:02:06 -0500

    added view controller snippet

---
+++ verticalScroll/BottomSheetViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BottomSheetViewController.swift
+//  verticalScroll
+//
+//  Created by Yasin Ehsan on 1/25/20.
+//  Copyright © 2020 Yasin Ehsan. All rights reserved.
+//
+
+import UIKit
+
+class BottomSheetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
